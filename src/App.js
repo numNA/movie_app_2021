71e703467d22ae8d0a7eafaaeb8a9cb227149f43
numNA <numna1018@gmail.com>
2021-07-27 03:10:57 +0900

    study_0726

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import react from 'react';
 import Potato from './Potato';
 
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>
+  );
+}
+
+const foodLike = [
+  {
+    name: 'Kimchi',
+    image: 'https://dimg.donga.com/wps/NEWS/IMAGE/2021/03/12/105850095.2.jpg', },
+  {
+    name: 'Jeyuk',
+    image: 'https://recipe1.ezmember.co.kr/cache/recipe/2015/05/27/38013d1dfd8fa46a871b9cda074b26341.jpg',},
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Potato />
+      {foodLike.map(dish => (<Food name={dish.name} picture={dish.image} />))}
     </div> 
   );
 }
